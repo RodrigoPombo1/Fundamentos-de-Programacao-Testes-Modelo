bc690f8e6fa88e42d4ff73324f5c93fb63a81474
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-11-28 19:44:28 +0000

    4funcionar ele n dizia q era [ ] para ( )maspronto

--- MT2P_M/Pergunta 4.py
+++ MT2P_M/Pergunta 4.py
@@ -1,7 +1,7 @@
 #não passa nos testes privados
 def unique_ntree(tree, result_list=[]):
     if len(tree) == 0:
-        return
+        return []
     if len(tree) == 1:
         result_list.append(tree[0])
     # if len(tree) == 3:
